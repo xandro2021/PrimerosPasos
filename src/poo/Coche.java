4e5e9787543f8afddb32952c5686160785fbeaa4
xandro2021 <xandrocastro2021@gmail.com>
2025-02-03 15:36:06 -0600

    Se crean y compilan las clases de prueba

---
+++ src/poo/Coche.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class Coche {
+  int ruedas, largo, ancho, motor, peso;
+
+  public Coche() {
+    ruedas = 4;
+    largo = 2000;
+    ancho = 300;
+    motor = 1600;
+    peso = 500;
+  }
+
+}
---
+++ src/poo/Uso_Coche.java
@@ -0,0 +1,13 @@
+package poo;
+
+/**
+ * Uso_Coche
+ */
+public class Uso_Coche {
+
+  public static void main(String[] args) {
+    Coche Renault = new Coche();
+    System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+  }
+
+}
